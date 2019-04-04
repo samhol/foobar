26321fafbf70401dcc774a0bf39412f2332f909f
samhol <sami.holck@gmail.com>
2019-04-04 14:55:12 +0300

    Ups

---
+++ samiholck/classes/Contact/Contact.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * SPHPlayground Framework (http://playgound.samiholck.com/)
+ *
+ * @link      https://github.com/samhol/SPHP-framework for the source repository
+ * @copyright Copyright (c) 2007-2019 Sami Holck <sami.holck@gmail.com>
+ * @license   https://opensource.org/licenses/MIT The MIT License
+ */
+
+namespace Sphp\Samiholck\Contact;
+
+use Sphp\Stdlib\Datastructures\Arrayable;
+use ArrayAccess;
+use IteratorAggregate;
+use Countable;
+use stdClass;
+
+/**
+ *
+ * @author  Sami Holck <sami.holck@gmail.com>
+ * @license https://opensource.org/licenses/MIT The MIT License
+ * @filesource
+ */
+class Contact {
+private $contacterMail;
+  /**
+   * Constructs a new instance
+   * 
+   * @param array $data
+   */
+  public function __construct() {
+
+  }
+
+  
+
+}
