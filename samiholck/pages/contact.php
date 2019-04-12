534b1a585e968c988827fcb422503818185aacf2
samhol <sami.holck@gmail.com>
2019-04-12 14:44:59 +0300

    Ups

--- samiholck/pages/contact.php
+++ samiholck/pages/contact.php
@@ -3,16 +3,15 @@
 
 \Sphp\Manual\md('# Contact Form');
 
-
-if (isset($_SESSION['contactFormResult'])) {
-  echo "<pre>SESSION:\n";
-  print_r($_SESSION['contactFormResult']);
-  echo '</pre>';
-}
-
 use Sphp\Samiholck\Contact\Controller;
 use Sphp\Stdlib\Datastructures\DataObject;
 use Sphp\Stdlib\Parsers\Parser;
+
+if (isset($_SESSION['contactFormResult'])) {
+  /* echo "<pre>SESSION:\n";
+    print_r($_SESSION['contactFormResult']);
+    echo '</pre>'; */
+}
 
 $config = DataObject::fromArray(Parser::fromFile('samiholck/config/private/contact-form.yml'));
 $controller = new Controller($config);
